6869b29c0e8d678acccf5670386e208cb579409e
Aurimar Salvador <aurimarsalvador@hotmail.com>
2026-09-14 19:37:10 -0300

    feat: initialize Norte backend

---
+++ src/main/java/br/com/norte/norte_api/controller/HealthController.java
@@ -0,0 +1,22 @@
+package br.com.norte.norte_api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthController {
+
+    @GetMapping
+    public Map<String, String> health() {
+        return Map.of(
+                "status", "UP",
+                "application", "Norte API",
+                "message","Ajudando jovens a encontrar caminhos para o futuro."
+        );
+    }
+}
---
+++ src/test/java/br/com/norte/norte_api/NorteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.norte.norte_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
